43abc3c2c821398ae0fadea6fd393c873e2a9758
Computer <Gavaharlal@yandex.ru>
2019-09-13 17:46:16 +0300

    added async downloader

--- app/src/main/java/com/dolgikh/myapp/SongListActivity.java
+++ app/src/main/java/com/dolgikh/myapp/SongListActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
